b1f18707688c9dc3f47042c5e794dadf40a42896
itjuh <jh.2144.9679@gmail.com>
2023-10-31 17:38:37 +0900

    63일차

--- 004.React학습/react-ex/js/make_data.js
+++ 004.React학습/react-ex/js/make_data.js
@@ -5,24 +5,41 @@
 // let gprice = ['49000','59000','69000','79000','89000'];
 
 // [여성의류 데이터 생성]
-let category = ["modern","casual"];
+
+// let category = ["modern","casual"];
+// let gname = ["워닝드레스","워스비버든","코코샤스넬라","포코로와드"];
+// let gprice = ["123000","224000","253000","340000","520000"];
+let category = ["관리자","방문객"];
 let gname = ["워닝드레스","워스비버든","코코샤스넬라","포코로와드"];
 let gprice = ["123000","224000","253000","340000","520000"];
+
 
 
 // rdm[아이템개수]
 let rdm = (x) => Math.floor(Math.random()*x);
 let bb = '';
-for(let x=0;x<19;x++){
+for(let x=0;x<120;x++){
 
 bb += `
     {
-        "idx":"${x}",
-        "gname":"${gname[rdm(4)]}",
-        "category":"${category[rdm(3)]}",
-        "gprice":"${gprice[rdm(5)]}",
+        "idx" : "${x+1}",
+        "tit" : "게시판 제목입니다${x+1}",
+        "cont" : "게시판 내용입니다${x+1}",
+        "att" : "",
+        "date" : "2023-${rdm(12)}-${rdm(30)}",
+        "writer" : "${category[rdm(2)]}",
+        "pwd" : "1111",
+        "cnt" : "${rdm(100)}"
     },
-`;
+ `;
+// bb += `
+//     {
+//         "idx":"${x}",
+//         "gname":"${gname[rdm(4)]}",
+//         "category":"${category[rdm(3)]}",
+//         "gprice":"${gprice[rdm(5)]}",
+//     },
+// `;
 }
 
 console.log(bb);
--- 005.jQuery학습/003.응용구현/jq_board/js/list.js
+++ 005.jQuery학습/003.응용구현/jq_board/js/list.js
@@ -10,18 +10,65 @@ import bData from './data.json' assert {type:'json'};
 const board = $('#board tbody');
 // 데이터 idx의 내림차순 정렬
 bData.sort((a,b)=>Number(a.idx) == Number(b.idx)? 0 : Number(a.idx)>Number(b.idx) ? -1: 1);
+// 리스트 번호변수
+let listNum = 0;
+// 리스트 번호증가 함수
+const addNum = () => ++listNum;
 
-board.html(
-    bData.map(v=>`
-    <tr>
-        <td>${v.idx}</td>
-        <td>${v.tit}</td>
-        <td>${v.writer}</td>
-        <td>${v.date}</td>
-        <td>${v.cnt}</td>
-    </tr>
-    `).join('') /////////// 맵조인
-);
+//////////////////////////////////////////////////////
+// 페이징되는 리스트 만들기 ////////////////
+// 페이징의 기본 원리 :
+// 1. 한페이지당 리스트 수를 정하여 전체 페이지 개수 구하기
+// 2. 개수만큼 리스트에 데이터를 반복문으로 생성 함
+// 3. 페이지 번호에 따라 시작 번호를 업데이트 한다!
+
+// [ 1. 변수설정 ]
+// 1) 한 페이지 당 리스트 수 : pgBlock
+const pgBlock = 10;
+// 2) 페이지 순번 : pgNum
+let pgNum = 1;
+// 3) 전체 레코드 수 : totalCnt
+const totalCnt = bData.length;
+// 4) 완전한 페이지 블록 = 내림(전체 레코드 / 페이지 당 리스트 수)
+let pagingBlock = Math.floor(totalCnt / pgBlock);
+// 5) 나머지 리스트 여부 : 0 추가페이지 없음 >0 추가페이지 개수
+let addOver = totalCnt % pgBlock;
+console.log(`pgBlock${pgBlock}:pgNum${pgNum}\ntotalCnt:${totalCnt}\npagingBlock:${pagingBlock}\naddOver:${addOver}`);
+
+// 시작번호 업데이트
+listNum = pgBlock*(pgNum-1);
+
+let hcode= '';
+// 리스트 블록으로 리스트 소스 만들기
+for(let i = pgBlock*(pgNum-1); i < pgBlock * pgNum; i++){
+    // 만약 리스트의 번호인 i가 전체 개수와 같거나 크면 break;
+    if(i >= totalCnt) break;
+    hcode +=`
+        <tr>
+            <td>${addNum()}</td>
+            <td>${bData[i].tit}</td>
+            <td>${bData[i].writer}</td>
+            <td>${bData[i].date}</td>
+            <td>${bData[i].cnt}</td>
+        </tr>
+    `
+
+} //////////////for///////////////
+
+// 코드 반영하기
+board.html(hcode);
+
+// board.html(
+//     bData.map(v=>`
+//     <tr>
+//         <td>${addNum()}</td>
+//         <td>${v.tit}</td>
+//         <td>${v.writer}</td>
+//         <td>${v.date}</td>
+//         <td>${v.cnt}</td>
+//     </tr>
+//     `).join('') /////////// 맵조인
+// );
 
 // 데이터를 태그로 구성
 // 태그 구조 : <tr>  <td></td>  </tr>
