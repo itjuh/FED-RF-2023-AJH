0f0e7e5e0cb517d58df4cd6979f392b996b6b39d
statusquo00 <jh.2144.9679@gmail.com>
2023-11-09 01:08:01 +0900

    제품 메인 디자인 1차완성

--- 800.개인프로젝트/02.SPA프로젝트/03.구현소스/js/prod_list.js
+++ 800.개인프로젝트/02.SPA프로젝트/03.구현소스/js/prod_list.js
@@ -53,30 +53,37 @@ $(() => {
     console.log('로드됨')
   const imgArrNum = [];
   // 랜덤 이미지 초기에 불러오기 함수
-  for (let i = 1; i < 11; i++) {
-    let randomNum = 0;
-    // 배열 내 랜덤 수 만들기 함수 /////////////////
-    function inputNum() {
-      randomNum = Math.floor(Math.random() * 48 + 1);
-      // 배열 내 랜덤 수 존재여부
-      let isIt = imgArrNum.find((v) => {
-        if (v == randomNum) return true;
-      });
-      if (!isIt) imgArrNum.push(randomNum);
-      else inputNum();
-      console.log(isIt,imgArrNum);
-    } ///////inputNum//////////
-    // 랜덤 수 만들기
-    inputNum();
-    // console.log(imgArrNum[i - 1]-1,prodData[imgArrNum[i - 1]-1][0]);
-    $(".prod-area ol").append(`
-      <li>    
-          <div class="prod-item" style="background-image: url(./images/image_prod2/keyboard${
-            imgArrNum[i - 1]
-          }.png)" data-seq='${imgArrNum[i - 1]-1}'>
-          </div>
-          <h3 class='prod-item-title'>${prodData[imgArrNum[i - 1]-1][0]}</h3>
-          <h3 class='prod-item-title'>${prodData[imgArrNum[i - 1]-1][1]}</h3>
-      </li>`);
-  } ///////////for문//////////////////////
+  imgSetting();
+  function imgSetting(){
+    for (let i = 1; i < 11; i++) {
+      let randomNum = 0;
+      // 배열 내 랜덤 수 만들기 함수 /////////////////
+      function inputNum() {
+        randomNum = Math.floor(Math.random() * 48 + 1);
+        // 배열 내 랜덤 수 존재여부
+        let isIt = imgArrNum.find((v) => {
+          if (v == randomNum) return true;
+        });
+        if (!isIt) imgArrNum.push(randomNum);
+        else inputNum();
+        console.log(isIt,imgArrNum);
+      } ///////inputNum//////////
+      // 랜덤 수 만들기
+      inputNum();
+      // console.log(imgArrNum[i - 1]-1,prodData[imgArrNum[i - 1]-1][0]);
+      $(".prod-area ol").append(`
+        <li>    
+            <div class="prod-item" style="background-image: url(./images/image_prod2/keyboard${
+              imgArrNum[i - 1]
+            }.png)" data-seq='${imgArrNum[i - 1]-1}'>
+            </div>
+            <h3 class='prod-item-title'>${prodData[imgArrNum[i - 1]-1][0]}</h3>
+            <h3 class='prod-item-title'>${prodData[imgArrNum[i - 1]-1][1]}</h3>
+            <!-- 위시리스트 버튼 -->
+            <div class='add-wish'>add to wishlist ＞</div>
+            <!-- 더보기 -->
+            <div class='prod-detail-view'>view</div>
+        </li>`);
+    } ///////////for문//////////////////////
+  }
 }); ////////////JQB///////////////////////////////////////
