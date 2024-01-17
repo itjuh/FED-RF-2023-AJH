0647e39fcbbbeeca2aa8ec4e83900a16eca14842
statusquo00 <jh.2144.9679@gmail.com>
2024-01-18 00:00:07 +0900

    개인페이지 수정

--- js/infoData copy.js
+++ js/infoData copy.js
@@ -1,12 +1,12 @@
 const infoData = {
     'About me' : ['한줄소개 여기에 쓸 것',],
-    'An JuHyun' : ['서울시 중랑구','Tel. 010.2144.9679','Email. jh.2144.9679@gmail.com','Github. https://github.com/itjuh',],
-    'Skills' : ['html,css','javascript,jquery','React','Flutter'],
+    'An JuHyun' : ['서울시 중랑구','Tel. 010.2144.9679','E. jh.2144.9679@gmail.com','Github. https://github.com/itjuh',],
+    'Skill' : ['html,css','javascript,jquery','React','Flutter'],
     'Education' : ['2020.11 ~ 2021.04','2023.07 ~ 2024.01 교육이수',],
 };
 const skillStack = {
     'html,css':['반응형 웹을 구성할 수 있습니다.','시맨틱 태그를 사용합니다.'],
-    'javascript,jquery':['함수를 통하여 동적으로 컨텐츠를 변경 할 수 있습니다.','비동기 프로그래밍을 구현할 수 있습니다.','JSON객체의 구문분석(parse)을 할 수 있습니다.'],
+    'javascript,jquery':['함수를 통하여 동적으로 컨텐츠를 변경 할 수 있습니다.','비동기 프로그래밍일 구현할 수 있습니다.','JSON객체의 구문분석(parse)을 할 수 있습니다.'],
     'React':['함수형 컴포넌트를 구성하여 웹 페이지를 구현할 수 있습니다.','Hook 상태관리 변수를 통하여 랜더링을 통제할 수 있습니다.'],
     'Flutter':['statelessWidget과 statefulWidget을 사용할 수 있습니다.','라우터 연결로 페이지를 이동할 수 있습니다.'],
 };
--- js/infomation.js
+++ js/infomation.js
@@ -2,18 +2,23 @@ import { infoData, skillStack } from "./infoData.js";
 
 console.log(infoData, skillStack);
 
-let htmlCode = '';
-for(let x in infoData){
+let htmlCode = "";
+for (let x in infoData) {
     htmlCode += `
     <div class="group-info ${x}">
     <div class="group-title">
     <h2>${x}</h2>
     </div>
     <ul>
-        ${infoData[x].map(v=>`<li>${v}</li>`).join('')}
+    ${
+        x === "Skills"
+            ? infoData[x]
+                  .map((v) => `<li><b>${v}</b> <ol>${skillStack[v].map((value) => `<li> - ${value}</li>`).join("")}</ol></li>`)
+                  .join("")
+            : infoData[x].map((v) => `<li>${v}</li>`).join("")
+    }
     </ul>
     </div>`;
 }
 
 document.querySelector(".info-me").innerHTML += htmlCode;
-
