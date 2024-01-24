4794aeecb98218d27426899b2dfeaaeb692d59f3
statusquo00 <jh.2144.9679@gmail.com>
2024-01-25 01:17:31 +0900

    개인페이지 정리

--- js/show_text.js
+++ js/show_text.js
@@ -1,23 +1,42 @@
 // 대상선정
 const textbox = document.querySelectorAll(".portfolio .show-text-box");
-const descbox1 = document.querySelector('#page-2 .pj-desc');
-const descbox2 = document.querySelector('#page-3 .pj-desc');
-const groupInfobox = $('.group-info');
+const descbox1 = document.querySelector("#page-2 .pj-desc");
+const descbox2 = document.querySelector("#page-3 .pj-desc");
+const groupInfobox = $(".group-info");
 const SHOWTIMEGAP = 1;
+const page1 = document.querySelector("#page-1");
 
-$(()=>{
-    if(pg_num == 0){
-        groupInfobox.addClass('on');
+$(() => {
+    if (pg_num == 0) {
+        groupInfobox.addClass("on");
     }
 }); //로드구역
-window.addEventListener("wheel", ()=>{
+// 휠 이벤트 - deskTop
+window.addEventListener("wheel", () => {
     console.log(pg_num);
-    if(pg_num == 0){
-        groupInfobox.addClass('on');
+    if (pg_num == 0) {
+        groupInfobox.addClass("on");
+        descbox1.classList.remove("on");
+        descbox2.classList.remove("on");
+    } else if (pg_num == 1) {
+        groupInfobox.removeClass("on");
+        descbox1.classList.add("on");
+        descbox2.classList.remove("on");
+    } else if (pg_num == 2) {
+        groupInfobox.removeClass("on");
+        descbox1.classList.remove("on");
+        descbox2.classList.add("on");
     }
-    else if(pg_num == 1){
-        descbox1.classList.add('on');
-    }else if(pg_num == 2){
-        descbox2.classList.add('on');
+});
+// 스크롤 이벤트 - mb
+window.addEventListener("scroll", function () {
+    if (page1.getBoundingClientRect().top > 0) {
+        groupInfobox.addClass("on");
+        descbox1.classList.remove("on");
+        descbox2.classList.remove("on");
+    } else {
+        groupInfobox.removeClass("on");
+        descbox1.classList.add("on");
+        descbox2.classList.add("on");
     }
 });
